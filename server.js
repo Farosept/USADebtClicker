d6aea280cb75dcd8014a6511c4b7354bfc62b45b
Артем Хорев <farosept@gmail.com>
2018-11-20 00:44:39 +0700

    Added odometer

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express"),
         path = require("path");
 app.listen(8080);   
 app.use(express.static(__dirname + '/View'));
+app.use("/public",express.static(__dirname + '/public'));
 app.get("/",(req, res)=>{
     res.sendFile("index.html");
 });
