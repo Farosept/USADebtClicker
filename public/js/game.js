d4a02c64171389c5c620168d291ae9215cb9cc8c
Artem <lobar.you@gmail.com>
2018-11-23 21:03:29 +0700

    Added level logic

--- public/js/game.js
+++ public/js/game.js
@@ -8,6 +8,18 @@ class Game{
     }
 
     countClicks(){
-        this.odometer.innerHTML = this.clicks++;
+        this.odometer.innerHTML = ++this.clicks;
+        this.levelUp();
+    }
+
+    levelUp(){
+        if(this.clicks>this.value+10){
+            document.getElementById("picture_lvl").src = "/public/pictures/test2.jpg";
+            document.getElementById("text_lvl").innerText = "Америка это просто печатный станок";
+        }
+        if(this.clicks>this.value+20){
+            document.getElementById("picture_lvl").src = "/public/pictures/test3.jpg";
+            document.getElementById("text_lvl").innerText = "Если вы посмотрите на графики — госдолг США идет вверх по экспоненте. В математике это называют режимом с обострением. Когда система входит в такой режим, она неизбежно разрушается. Американской гегемонии все равно придет конец — это объективный закон";
+        }
     }
 }
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express"),
     app = express();
-app.listen(8080);
+app.listen(8080,()=>{
+    console.log("http://localhost:8080");
+});
 app.use(express.static(__dirname + "/View"));
 app.use("/public",express.static(__dirname + "/public"));
 app.get("/", (req, res)=>{
