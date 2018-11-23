abff3cf18b94148379aee9704c5ddc10c22a3604
Artem <lobar.you@gmail.com>
2018-11-23 17:45:17 +0700

    Added new architecture

---
+++ public/js/game.js
@@ -0,0 +1,13 @@
+class Game{
+    constructor(odometer, value){
+        this.value = value;
+        this.clicks = value;
+        this.odometer = odometer;
+        this.odometer.innerHTML = value;
+        this.odometer.addEventListener("click",this.countClicks.bind(this));
+    }
+
+    countClicks(){
+        this.odometer.innerHTML = this.clicks++;
+    }
+}
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require("express"),
-        app=express(),
-        path = require("path");
-app.listen(8080);   
-app.use(express.static(__dirname + '/View'));
-app.use("/public",express.static(__dirname + '/public'));
-app.get("/",(req, res)=>{
+var express = require("express"),
+    app = express();
+app.listen(8080);
+app.use(express.static(__dirname + "/View"));
+app.use("/public",express.static(__dirname + "/public"));
+app.get("/", (req, res)=>{
     res.sendFile("index.html");
 });
