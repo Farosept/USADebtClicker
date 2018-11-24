50001124d849d74f3774a0055e27d5e5a814a794
Artem <lobar.you@gmail.com>
2018-11-24 14:10:01 +0700

    Added styles for lvl block

--- public/js/game.js
+++ public/js/game.js
@@ -15,11 +15,13 @@ class Game{
     levelUp(){
         if(this.clicks>this.value+10){
             document.getElementById("picture_lvl").src = "/public/pictures/test2.jpg";
-            document.getElementById("text_lvl").innerText = "Америка это просто печатный станок";
+            document.getElementById("text_lvl").innerText = "Америка это просто печатный станок!";
+            document.getElementById("head_lvl").innerText = "Ватник продвинутый";
         }
         if(this.clicks>this.value+20){
             document.getElementById("picture_lvl").src = "/public/pictures/test3.jpg";
             document.getElementById("text_lvl").innerText = "Если вы посмотрите на графики — госдолг США идет вверх по экспоненте. В математике это называют режимом с обострением. Когда система входит в такой режим, она неизбежно разрушается. Американской гегемонии все равно придет конец — это объективный закон";
+            document.getElementById("head_lvl").innerText = "Ватник-пассионарий";
         }
     }
 }
