75a6b3f36fde5ff4d18747496fc879264ac0c0b4
Unknown <exanimatesi@yandex.ru>
2019-01-08 22:24:50 +0700

    Changed indent in .eslintrc

--- public/js/game.js
+++ public/js/game.js
@@ -1,16 +1,16 @@
-class Game{
-    constructor(odometer, value){
-        this.value = value;
-        this.clicks = value;
-        this.odometer = odometer;
-        this.odometer.innerHTML = value;
-        this.odometer.addEventListener("click",this.countClicks.bind(this));
-    }
+class	Game{
+	constructor(odometer, value){
+		this.value = value;
+		this.clicks = value;
+		this.odometer = odometer;
+		this.odometer.innerHTML = value;
+		this.odometer.addEventListener("click",this.countClicks.bind(this));
+	}
 
-    countClicks(){
-        this.odometer.innerHTML = ++this.clicks;
-        this.levelUp();
-    }
+	countClicks(){
+		this.odometer.innerHTML = ++this.clicks;
+		this.levelUp();
+	}
 
     levelUp(){
         if(this.clicks>this.value+10){
